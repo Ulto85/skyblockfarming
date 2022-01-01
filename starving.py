5c15e609858730c317e39dd2845b33856d9bf767
Ulto85 <74577196+Ulto85@users.noreply.github.com>
2022-01-01 14:16:54 -0800

    Add files via upload

---
+++ starving.py
@@ -0,0 +1,27 @@
+import webbot
+import requests
+from bs4 import BeautifulSoup
+import time
+
+food= webbot.Browser()
+print(1)
+food.go_to('google.com')
+
+while True:
+    #print(food.get_current_url())
+    
+    if 'doordash' in food.get_current_url() or "ubereats" in food.get_current_url() or "grubhub" in food.get_current_url():
+        pg = requests.get('https://sky.shiiyu.moe/stats/CoolDabber/Kiwi')
+        supper = BeautifulSoup(pg.content,'lxml')
+        oofs = supper.find_all('div',{'class':'skill-name'})
+        farm = [oof for oof in oofs if 'Farming' in oof.get_text()]
+        lvl = farm[0].find('span').get_text()
+        if int(lvl)>=18:
+            pass
+        else:
+            #food.close_current_tab()
+            food.go_to('google.com')
+            food.execute_script(f"document.getElementsByTagName('body')[0].innerHTML='FARMER JOE:You only have {lvl} out of 18 farming levels required to eat';")
+            
+         
+
