8cb0c6d4d5a1676cd6ecb5688f187833a542ca1c
Ulto85 <74577196+Ulto85@users.noreply.github.com>
2022-01-01 14:19:05 -0800

    Update starving.py

--- starving.py
+++ starving.py
@@ -6,22 +6,23 @@ import time
 food= webbot.Browser()
 print(1)
 food.go_to('google.com')
-
+YOUR_USERNAME = "" #ex. CoolDabber
+YOUR_LEVEL_REQUIREMENT = 18 
 while True:
     #print(food.get_current_url())
     
     if 'doordash' in food.get_current_url() or "ubereats" in food.get_current_url() or "grubhub" in food.get_current_url():
-        pg = requests.get('https://sky.shiiyu.moe/stats/{YOUR USERNAME}/Kiwi')
+        pg = requests.get(f'https://sky.shiiyu.moe/stats/{YOUR_USERNAME}/Kiwi')
         supper = BeautifulSoup(pg.content,'lxml')
         oofs = supper.find_all('div',{'class':'skill-name'})
         farm = [oof for oof in oofs if 'Farming' in oof.get_text()]
         lvl = farm[0].find('span').get_text()
-        if int(lvl)>=18:#or any other level requirement
+        if int(lvl)>=YOUR_LEVEL_REQUIRMENT:
             pass
         else:
             #food.close_current_tab()
             food.go_to('google.com')
-            food.execute_script(f"document.getElementsByTagName('body')[0].innerHTML='FARMER JOE:You only have {lvl} out of {YOUR LEVEL REQUIREMENT} farming levels required to eat';")
+            food.execute_script(f"document.getElementsByTagName('body')[0].innerHTML='FARMER JOE:You only have {lvl} out of {YOUR_LEVEL_REQUIREMENT} farming levels required to eat';")
             
          
 
